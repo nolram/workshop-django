3545a01dca5ce16fd132c0de8624b560742f1b36
Marlon Baptista de Quadros <marlon.quadros@southsystem.com.br>
2020-02-21 17:36:05 -0300

    - Sistema de login

--- issues_manager/views.py
+++ issues_manager/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
+
 from django.http import HttpResponse
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def pagina_inicial(request):
     # GET, POST, PUT, DELETE, PATCH
     return HttpResponse("Hello World")
